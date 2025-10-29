57878dd8c6e8c345ae37d5eb8b85ad7a2abe8a15
Maeren Hay <148922167+mch555@users.noreply.github.com>
2025-10-29 18:47:13 -0500

    Add Fibonacci calculation methods
    Implemented Fibonacci functions: recursive, top-down, and bottom-up approaches.

--- main.py
+++ main.py
@@ -5,20 +5,48 @@ def fib_recursive(n, counts):
     each time fib_recursive(i, counts) is called.
     """    
     counts[n] += 1
-    ###TODO
-    pass
+    #base cases
+    if n==0:
+        return 0
+    if n==1:
+        return 1
+    #recursive step
+    return fib_recursive(n-1, counts)+fib_recursive(n-2,counts)
+    
     
 
     
 def fib_top_down(n, fibs):
-    ###TODO
-    pass
+    #if the val has already been computed, return it
+    if fibs[n] != -1:
+        return fibs[n]
+    #base cases
+    if n==0:
+        fibs[n]=0
+        return 0
+    if n==1:
+        fibs[n]=1
+        return 1
+    #recursive step
+    result= fib_top_down(n-1, fibs)+ fib_top_down(n-2, fibs)
+    fibs[n]=result
+    return result
 
 
 def fib_bottom_up(n):
-    ###TODO
-    pass
-
-
-
+    #base cases
+    if n==0:
+        return 0
+    if n==1:
+        return 1
+    
+    #Initialize array to hold fib values
+    fibs= [0]*(n+1)
+    #set base cases
+    fibs[0]=0
+    fibs[1]=1
+    #fill rest of array
+    for i in range(2, n+1):
+        fibs[i]=fibs[i-1]+fibs[i-2]
+    return fibs[n]
 
